15e31dfd279381ffafb285189d87403f5d394f81
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2025-05-07 14:43:16 +0000

    Added a dynamodb subservice for database related operations required

--- services/image-processing/src/sub-services/dynamodbService.js
+++ services/image-processing/src/sub-services/dynamodbService.js
@@ -1,2 +1,45 @@
-const { DynamoDBDocumentClient, PutCommand, GetCommand, UpdateCommand, QueryCommand, ScanCommand } = require('@aws-sdk/lib-dynamodb');
+const { DynamoDBDocumentClient, PutCommand, GetCommand, UpdateCommand, DeleteCommand, QueryCommand, ScanCommand } = require('@aws-sdk/lib-dynamodb');
 const { dynamoClient } = require('../config/aws');
+
+const docClient = DynamoDBDocumentClient.from(dynamoClient);
+const TABLE_NAME = process.env.DYNAMODB_TABLE;
+
+class DynamoService {
+    static async createImageMetadata(item) {
+        const command = new PutCommand({
+            TableName: TABLE_NAME,
+            Item: item
+        });
+        return docClient.send(command);
+    }
+
+    static async getImageMetadata(imageId) {
+        const command = new GetCommand({
+            TableName: TABLE_NAME,
+            Key: { imageId }
+        });
+        return docClient.send(command);
+    }
+
+    static async updateImageMetadata(imageId, updateExpression, expressionAttributeValues) {
+        const command = new UpdateCommand({
+            TableName: TABLE_NAME,
+            Key: { imageId },
+            UpdateExpression: updateExpression,
+            ExpressionAttributeValues: expressionAttributeValues,
+            ReturnValues: 'ALL_NEW'
+        });
+        return docClient.send(command);
+    }
+
+
+    static async deleteImageMetadata(imageId) {
+        const command = new DeleteCommand({
+            TableName: TABLE_NAME,
+            Key: { imageId }
+        });
+        return docClient.send(command);
+    }
+}
+
+module.exports = DynamoService;
