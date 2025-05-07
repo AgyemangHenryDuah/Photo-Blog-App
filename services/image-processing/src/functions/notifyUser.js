353f768a7dba5fe355373c847f333f0e94e5af01
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2025-05-07 17:17:53 +0000

    Added workflow via comments to lambda functions

---
+++ services/image-processing/src/functions/notifyUser.js
@@ -0,0 +1,7 @@
+// Get user email from database
+
+
+//Generate email subject
+
+
+//Send email to user
---
+++ services/image-processing/src/functions/watermarkImage.js
@@ -0,0 +1,9 @@
+// Get the uploaded image from S3
+
+// Apply watermark
+
+// Upload watermarked image to final bucket
+
+// Update metadata
+
+//Send success notification
