ba0c1e02259e7e7da77cccd56cd4329ba6b4216e
johnkponyo <john.kponyo@amalitech.com>
2025-05-08 00:17:10 +0000

    Configured an independent testing and deployment for photo-management service

--- services/photo-management/src/get-upload-url/index.js
+++ services/photo-management/src/get-upload-url/index.js
@@ -1,1 +1,17 @@
-// Implement Lambda to generate presigned URL accordingly
+
+exports.handler = async (event) => {
+
+    console.log('HURRAY...!!! Let the work begin <----->', event);
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            info: 'It worked!', 
+            message: 'Welcome...! This is PBA :)'
+        })
+    }
+}
+
