a99432b78f1e7cb95fac431bf8367f457d575530
Lotsu Daniel Ajayi <daniel.lotsu@amalitech.com>
2025-05-08 16:01:16 +0000

    modified the returns of the message handlers and also modified the email templates

--- services/image-processing/src/functions/send-success-email.js
+++ services/image-processing/src/functions/send-success-email.js
@@ -1,28 +1,41 @@
 exports.handler = async (event) => {
-    try {
-      const { imageId, errorMessage } = JSON.parse(event.body);
-  
-      // Get user email from DynamoDB
-      const userEmail = await processingService.getUserEmail(imageId);
-  
-      if (!userEmail) {
-        throw new Error("User email not found for image: " + imageId);
-      }
-  
-      // Send processing failed email
-  
-      await emailService.sendEmail(userEmail, "processing_success", {
-        imageId,
-        successTime: new Date().toISOString(),
-      });
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Processing failed email sent" }),
-      };
-    } catch (error) {
-      console.error("Error in processingFailed:", error);
-      throw error;
+  try {
+    const { userInfo, imageInfo } = JSON.parse(event.body);
+
+    // Get user email from DynamoDB
+    const userEmail = userInfo.email;
+    const firstName = userInfo.firstname;
+
+    //Get image metadata
+    const imageId = imageInfo.imageId;
+    const imageName = imageInfo.imageName;
+    const imageUrl  = imageInfo.imageUrl;
+
+    if (!userEmail) {
+      throw new Error("User email not found for image: " + imageName);
     }
-  };
-  
+
+    // Send processing failed email
+    await emailService.sendEmail(userEmail, "processing_success", {
+      firstName,
+      imageName,
+      completedAt: new Date().toISOString(),
+      downloadUrl: imageUrl
+    });
+
+    return {
+      statusCode: 200,
+      message: JSON.stringify({ message: "Successfully processed email sent" }),
+      body: {
+        Notification: true,
+      },
+    };
+  } catch (error) {
+    console.error("Error in processingFailed:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error sending email" }),
+      notification: false,
+    };
+  }
+};
