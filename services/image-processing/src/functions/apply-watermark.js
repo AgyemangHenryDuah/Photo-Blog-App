4482684108889816f9c2f9e5f3c7e913ca7f31c9
danielsahkwadwo <daniel.sah@amalitech.com>
2025-05-08 14:19:58 +0000

    added watermarking functionality

--- services/image-processing/src/functions/apply-watermark.js
+++ services/image-processing/src/functions/apply-watermark.js
@@ -1,9 +1,34 @@
-// Get the uploaded image from S3
+const { s3Service } = require("../sub-services/s3Service");
+const { getTextBuffer, applyWatermark, streamToBuffer } = require("../Utils/watermarkImage");
 
-// Apply watermark
+exports.handler = async (event) => {
+  try {
+    const { body, key } = event;
 
-// Upload watermarked image to final bucket
+    //get image
+    const imageData = await s3Service.uploadFromStaging(key);
 
-// Update metadata
+    //create watermark text
+    const uploadDate = new Date(Date.now()).toLocaleString("en-US", { month: "long", day: "numeric", year: "numeric" });
+    const fullname = `${userData.firstname} ${userData.lastname}`;
+    const watermarkText = `${fullname} - ${uploadDate}`;
 
-//Send success notification
+    //stream image to buffer
+    const streamedImage = await streamToBuffer(imageData);
+
+    //create a watermark buffer
+    const textSvg = await getTextBuffer(watermarkText);
+
+    //Apply watermark to image
+    const watermarkedImage = await applyWatermark(streamedImage, textSvg);
+
+    //upload to asset bucket
+    const outputKey = await s3Service.uploadToAssetBucket(watermarkedImage, key);
+    const imageUrl = `https://${process.env.ASSET_BUCKET_NAME}.s3.amazonaws.com/${outputKey}`;
+
+    console.log("watermark applied successfully");
+  } catch (error) {
+    console.error(error);
+    throw new Error("there was an image when applying watermark");
+  }
+};
