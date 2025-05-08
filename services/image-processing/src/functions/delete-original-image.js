320751656bd0384f3e4bf0e17aa7ca0d6e4cb0db
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2025-05-08 10:53:46 +0000

    Added a lmbda to delete original image from s3 after processing

--- services/image-processing/src/functions/delete-original-image.js
+++ services/image-processing/src/functions/delete-original-image.js
@@ -1,2 +1,19 @@
-// Delete the original uploaded file
+const { s3Service } = require('../sub-services/s3Service');
 
+module.exports.handler = async (event) => { 
+    try {
+        const { key } = event
+        await s3Service.deleteFromStaging(key)
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Image deleted successfully" })
+        };    
+    } catch (error) {
+        console.error("Error in deleteOriginalImage:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal Server Error" })
+        };
+    }
+}
