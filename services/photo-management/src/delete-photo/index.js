1438ab0d0c9ca4d2932c075322d6a4e1028d3aaa
emmanuelasidigbe <emmanuel.asidigbe@amalitech.com>
2025-05-09 12:28:49 +0000

    add implentent on routes of photo-management-mcs

--- services/photo-management/src/delete-photo/index.js
+++ services/photo-management/src/delete-photo/index.js
@@ -1,1 +1,56 @@
-// Implement Lambda to delete photo (moves to recycle bin) accordingly
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  UpdateCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.handler = async (event) => {
+  try {
+    const { imageId } = JSON.parse(event.body || "{}");
+    const tableName = process.env.PHOTOS_TABLE;
+
+    const userId =
+      event.requestContext?.authorizer?.claims?.sub ||
+      "1324d8c2-8091-7086-a944-773d576f9eea"; // REST API fallback
+
+    if (!imageId || !userId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Missing imageId or userId" }),
+      };
+    }
+
+    const timestamp = Date.now();
+
+    const command = new UpdateCommand({
+      TableName: tableName,
+      Key: {
+        imageId,
+        userId,
+      },
+      UpdateExpression: "SET isDeleted = :true, deletedAt = :timestamp",
+      ExpressionAttributeValues: {
+        ":true": true,
+        ":timestamp": timestamp,
+      },
+      ConditionExpression:
+        "attribute_exists(imageId) AND attribute_exists(userId)",
+    });
+
+    await docClient.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Photo deleted (soft delete)." }),
+    };
+  } catch (err) {
+    console.error("Error deleting photo:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to delete photo." }),
+    };
+  }
+};
